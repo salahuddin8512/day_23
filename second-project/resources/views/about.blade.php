ad35a04bef2d46d8ce95587522a91f440c872542
salahuddin8512 <salahuddin8512@gmail.com>
2022-02-16 13:11:12 +0600

    d

---
+++ second-project/resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+@section('title')
+    About Page
+@endsection
+@section('body')
+    <h1>About Page</h1>
+@endsection
---
+++ second-project/resources/views/bitm.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+@section('title')
+    BITM Page
+@endsection
+@section('body')
+    <h1>BITM Page</h1>
+@endsection
